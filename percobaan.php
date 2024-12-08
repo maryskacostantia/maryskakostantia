0c016667508c1d9a0235531ef898e571cf2bd1ff
maryskacostantia <tiahsb12@gmail.com>
2024-12-09 00:13:35 +0700

    Update percobaan.php

--- percobaan.php
+++ percobaan.php
@@ -3,3 +3,9 @@ echo "Rayanti Utami " ;
 print "141401042 ";
 ?>
 
+<?php
+// Loop untuk menampilkan pernyataan hari belajar PHP
+for ($i = 1; $i <= 5; $i++) {
+    echo "Ini adalah hari ke-$i belajar PHP<br>";
+}
+?>
