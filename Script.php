99618be757e8f88db565b906c3f0c44ba87cc5d8
maryskacostantia <tiahsb12@gmail.com>
2024-12-09 01:25:35 +0700

    Update Script.php

--- Script.php
+++ Script.php
@@ -4,3 +4,20 @@ for ($i = 1; $i <= 5; $i++) {
     echo "Ini adalah hari ke-$i belajar PHP<br>";
 }
 ?>
+
+<form action="" method="POST">
+<input type="text" name="angkal" size="2"> +
+<input type="text" name="angka2" size="2">
+<input type="submit" value="Proses" name="hitung">
+</form>
+
+Hasilnya adalah : |
+<?php
+
+if(isset($_POST['hitung'])){
+$angka1=$_POST['angka1'];
+$angka2=$_POST['angka2'];
+
+$hasil=$angka1 + $angka2;
+echo $hasil;
+}
